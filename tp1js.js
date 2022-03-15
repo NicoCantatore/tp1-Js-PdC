3ab348fe95556a19b6398f1ac15f7a2a4ae2d4db
NicoCantatore <nicolas.cantatore@gmail.com>
2022-03-15 20:40:56 -0300

    v1

---
+++ tp1js.js
@@ -0,0 +1,10 @@
+// 1- Deberán mostrar por consola los siguientes datos:
+// a) Mostrar el día de tu cumpleaños (en numero) Ej: 23
+// b) Mostrar el mes de tu cumpleaños (en numero) Ej: 07
+// c) Mostrar el año de tu nacimiento.
+// d) Mostrar tu número favorito.
+
+console.log(6);
+console.log(2);
+console.log(1987);
+console.log(15);
